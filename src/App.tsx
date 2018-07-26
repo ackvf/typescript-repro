1687ff8890c28694e9c61834e12a172405554387
Vítězslav Ackermann <vitezslavferko@gmail.com>
2018-07-26 14:44:05 +0200

    SETUP reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from 'react'
+import './App.css'
 
 class App extends React.Component {
+  public state: {
+    num: number
+    records: {}
+  } = {
+    num: 0,
+    records: {},
+  }
+
+  public mergeIntoState = () => {
+    const _records = {} // tslint:disable-line:variable-name
+
+    ;['a', 'b', 'c'].forEach(el => {
+      _records[el] = {}
+    })
+
+
+    // issue 1
+
+    this.setState(({num}) => ({num: num + 1})) // var num: this['state']['num']
+    this.setState(({num}) => ({num: num + ''})) // var num: any
+    //            ({any})  => ({string: any + string})
+
+
+    // issue 2
+
+    this.setState(({records}) => ({records: {..._records, ...records}})) // Spread types may only be created from object types.
+    this.setState(({records}) => ({records: {..._records, ...(records as this['state']['records'])}})) // Spread types may only be created from object types.
+    //            ({any})     => ({any:     {...{},       ...(any     as  (property) records: {} )}})
+    this.setState(({records}) => ({records: {..._records, ...(records as {})}})) // this is ok
+    //            ({this["state"]["records"]})
+  }
+
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    return null
   }
 }
 
-export default App;
+export default App
+
+
+// for simplicity, a significantly reduced signature
+declare module 'React' {
+  interface Component { // tslint:disable-line:interface-name
+    setState<K extends keyof T, T = this['state']>(
+        state: (
+          (prevState: Readonly<T>) => (Pick<T, K> | T | null)
+        )
+    ): void
+  }
+}
